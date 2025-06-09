b22544794315fd6660bb64b4c948064af8f72005
multivariablecalculus <subhodisha2062@gmail.com>
2025-06-09 16:26:12 +0530

    Maintainance

--- obj/Debug/net9.0/SysMonitor.AssemblyInfo.cs
+++ obj/Debug/net9.0/SysMonitor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SysMonitor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2a2718b8519368e8bfae710e53a7b407674e06b")]
 [assembly: System.Reflection.AssemblyProductAttribute("SysMonitor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SysMonitor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
